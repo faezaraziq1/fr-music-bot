c047e576d44329f24eeead081b26ee9c8e8d453d
faezaraziq1 <47936980+faezaraziq1@users.noreply.github.com>
2021-03-12 20:05:03 +0700

    Delete commands directory

--- commands/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = (client, message, args, queue, searcher) => {
-    message.channel.send("Hello!!");
-}
-
-module.exports.config = {
-    name: "hello",
-    aliases: ["hi", "hey", "yo"]
-}
